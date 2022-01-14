f12a30c9a98689ae7cceba5b5627145a9af97294
joochul <joochul@aniai.ai>
2022-01-14 22:32:27 +0900

    gonna make library here

---
+++ STM32LIB-LWIP-HTTPclient/Target/httpc_conf.h
@@ -0,0 +1,11 @@
+#ifndef __HTTPC_CONF_H
+#define __HTTPC_CONF_H
+
+#define SERVER_IP_ADDR0   (uint8_t) 192
+#define SERVER_IP_ADDR1   (uint8_t) 168
+#define SERVER_IP_ADDR2   (uint8_t) 1
+#define SERVER_IP_ADDR3   (uint8_t) 192
+
+#define SERVER_PORT       (uint16_t) 5050
+
+#endif
---
+++ STM32LIB-LWIP-HTTPclient/Target/httpc_platform.h
@@ -0,0 +1,15 @@
+#ifndef __HTTPC_PLATFORM_H
+#define __HTTPC_PLATFORM_H
+
+
+
+#include "lwip/tcp.h"
+#include "httpc_conf.h"
+
+#include <stdbool.h>
+
+
+
+
+
+#endif /* __HTTPC_PLATFORM_H */
